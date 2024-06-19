4ade2acc8622e9ea3787540aa83cbb57185227a3
DWC97 <dwcarton1@gmail.com>
2024-06-19 16:36:12 +0100

    lottie scroll animation

--- src/components/home/hero/Hero.jsx
+++ src/components/home/hero/Hero.jsx
@@ -6,6 +6,8 @@ import RenderModel from './RenderModel';
 import DecodeAnimation from 'react-decode-animation';
 import { motion } from 'framer-motion';
 import { TypeAnimation } from 'react-type-animation';
+import Lottie from 'lottie-react';
+import animationData from '@/animations/scroll-animation.json';
 
 export default function Hero() {
     const nameRef = useRef(null);
@@ -13,6 +15,8 @@ export default function Hero() {
     const [modelReady, setModelReady] = useState(false);
     const [divVisible, setDivVisible] = useState(false);
     const [textVisible, setTextVisible] = useState(false);
+    const [scrollAnimation, setScrollAnimation] = useState(false);
+    const scrollAnimationRef = useRef(null);
 
     useEffect(() => {
         if (modelReady) {
@@ -25,7 +29,10 @@ export default function Hero() {
                     nameRef.current.play();
                     setTextVisible(true);
                 }
-            }, 750); // Adjust this delay as needed
+            }, 750); 
+            setTimeout(() => {
+                setScrollAnimation(true)
+            }, 5000);
         }
     }, [modelReady]);
 
@@ -36,7 +43,7 @@ export default function Hero() {
                     initial={{ width: 0 }}
                     animate={{ width: '784px' }}
                     transition={{ duration: 1.5, ease: [0.4, 0, 0.1, 1] }}
-                    className=" h-[336px] z-10 backdrop-blur-sm relative flex flex-col px-8 overflow-hidden "
+                    className=" h-[336px] z-10 backdrop-blur-sm relative flex flex-col px-8 overflow-hidden select-none"
                 >
                     <div className="absolute top-0 left-0 z-10 w-full h-full bg-gradient-to-b from-med-blue to-[#143A3A] opacity-10" />
                     <div className="absolute top-0 left-0 w-full">
@@ -52,12 +59,12 @@ export default function Hero() {
                     </div>
 
                     <h1
-                        className={`z-20 font-medium text-[116px]  mt-[36px] -mb-6 -ml-2 ${textVisible ? 'text-white weak-glow' : 'text-transparent'} ease-in-out transition-colors duration-300`}
+                        className={`z-20 font-medium text-[116px]  mt-[36px] -mb-6 -ml-2 ${textVisible ? 'text-white ' : 'text-transparent'} ease-in-out transition-colors duration-300`}
                     >
                         Developer
                     </h1>
                     <h1
-                        className={`z-20 font-medium text-[116px] -my-6 -ml-2 ease-in-out transition-colors duration-500 ${textVisible ? 'text-white weak-glow' : 'text-transparent'}`}
+                        className={`z-20 font-medium text-[116px] -my-6 -ml-2 ease-in-out transition-colors duration-500 ${textVisible ? 'text-white ' : 'text-transparent'}`}
                     >
                         <span
                             className={`${textVisible ? 'text-custom-gray' : 'text-transparent'} ease-in-out transition-colors duration-500`}
@@ -87,11 +94,21 @@ export default function Hero() {
                 </motion.div>
             )}
 
-            <div className="absolute top-0 left-0 opacity-60 w-full h-screen z-0">
+            <div className="absolute top-0 left-0 opacity-80 w-full h-screen z-0">
                 <RenderModel setModelReady={setModelReady}>
                     <Digital />
                 </RenderModel>
             </div>
+            <div
+                    className={`absolute left-1/2 top-1/2 translate-x-[-50%] translate-y-[200px] ${scrollAnimation ? ' opacity-80' : 'opacity-0'} flex justify-center items-center ease-in-out duration-1000 z-30`}
+                >
+                    <Lottie
+                        lottieRef={scrollAnimationRef}
+                        animationData={animationData}
+                        className="w-16"
+                        loop={true}
+                    />
+                </div>
         </div>
     );
 }

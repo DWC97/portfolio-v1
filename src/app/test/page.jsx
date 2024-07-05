b2a288e64411e9acc17bc9197a1dfc093782ed74
DWC97 <dwcarton1@gmail.com>
2024-07-05 10:36:44 +0100

    test page

---
+++ src/app/test/page.jsx
@@ -0,0 +1,11 @@
+import Hero from "@/components/home/hero/Hero";
+import ProjectSection from "@/components/home/project/ProjectSection";
+
+export default function Test(){
+    return (
+        <div className="w-full min-h-[200vh]">
+            <Hero />
+            {/* <ProjectSection /> */}
+        </div>
+    )
+}

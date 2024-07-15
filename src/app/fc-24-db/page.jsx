a04cb1562a87257d4848548a3adb3e0b84acd225
DWC97 <dwcarton1@gmail.com>
2024-07-15 16:16:24 +0100

    responsive banner

--- src/app/fc-24-db/page.jsx
+++ src/app/fc-24-db/page.jsx
@@ -4,11 +4,11 @@ export default function FC24DB() {
     return (
         <div className="w-full h-[200vh] flex flex-col items-center relative">
         
-            <div className="max-w-[1160px] w-full px-20  -mb-10">
+            <div className="max-w-[1160px] w-full px-10 sm:px-20 mb-10 sm:-mb-10">
                 <Banner />
             </div>
       
-            <div className='max-w-[1160px] w-full px-20   z-30'>
+            <div className='max-w-[1160px] w-full px-10 sm:px-20   z-30'>
 
             
             <p className='font-medium text-[20px] text-gray-200'>I built the front-end in React.js with Tailwind for styling. The minor back-end component is an Express server used to scrape player images from an existing database. </p>
--- src/components/project-page/Banner.jsx
+++ src/components/project-page/Banner.jsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import { motion } from 'framer-motion';
 import projectsData from '@/data/projects.json';
 import Image from 'next/image';
 
@@ -22,8 +21,8 @@ export default function Banner() {
     }, []);
 
     return (
-        <div className="w-full h-[700px] relative overflow-hidden">
-            <div className="relative h-[600px] w-full  z-0"
+        <div className="w-full h-auto sm:h-[1040px] xl:h-[700px] relative overflow-hidden">
+            <div className="relative hidden sm:flex h-[500px] md:h-[600px] w-full  z-0"
             style={{ opacity }}
             >
                 <Image
@@ -35,12 +34,67 @@ export default function Banner() {
                     className="object-contain opacity-90 z-0 "
                 />
             </div>
-            <div className="absolute bottom-0 left-0 w-full h-[250px] bg-primary-dark z-10" />
-            <div className="absolute bottom-[250px] left-0 w-full h-[300px] bg-gradient-to-b from-transparent to-primary-dark z-10" />
-            <div className="absolute bottom-[250px] left-0 w-full h-[320px] bg-gradient-to-b from-transparent to-primary-dark z-10" />
-            <div className="absolute top-[320px] w-full flex flex-row gap-20 items-center z-20">
-                <div className="flex flex-col w-[620px]">
-                    <h1 className="text-white text-[56px] font-semibold">
+            <div className="absolute hidden sm:flex top-[450px] xl:bottom-0 left-0 w-full h-[250px] bg-primary-dark z-10" />
+            <div className="absolute hidden sm:flex bottom-[590px] xl:bottom-[250px] left-0 w-full h-[300px] bg-gradient-to-b from-transparent to-primary-dark z-10" />
+            <div className="absolute hidden sm:flex bottom-[590px] xl:bottom-[250px] left-0 w-full h-[320px] bg-gradient-to-b from-transparent to-primary-dark z-10" />
+            <div className=" sm:absolute relative sm:mt-0 mt-[160px] flex  sm:top-[320px] w-full  flex-col xl:flex-row gap-10 xl:gap-20 items-center z-20">
+                <div className="flex flex-col w-full xl:w-[620px]">
+                    <h1 className="text-white text-[40px] md:text-[56px] font-semibold">
+                        FC 24 Player Database
+                    </h1>
+                    <p className="font-medium text-[20px] text-custom-gray mt-8">
+                        Search & visualization tool for the FC 24 player
+                        dataset. Includes ~5000 players from Europe's top five
+                        leagues. Non-commerical product created purely for skill
+                        development.
+                    </p>
+                    <div className="flex flex-row justify-between items-center mt-8">
+                        <div className="flex flex-row items-center gap-3 hover:opacity-80 ease-in-out duration-300">
+                            <span className="text-dark-blue font-medium text-[20px]">
+                                Live demo
+                            </span>
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width={24}
+                                height={24}
+                                viewBox="0 0 16 16"
+                                className='hidden sm:flex'
+                            >
+                                <path
+                                    fill="#00EEFF"
+                                    fillRule="evenodd"
+                                    d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8"
+                                ></path>
+                            </svg>
+                        </div>
+                        <ul className="flex flex-row ">
+                            {project.stack.map((stack, i) => {
+                                return (
+                                    <li
+                                        key={i}
+                                        className="-mr-2 last:mr-0 h-[48px] w-[48px] flex justify-center items-center rounded-full border border-custom-gray border-opacity-30 bg-gradient-to-r from-primary-light to-primary-dark"
+                                    >
+                                        <div
+                                            dangerouslySetInnerHTML={{
+                                                __html: stack.svg,
+                                            }}
+                                        />
+                                    </li>
+                                );
+                            })}
+                        </ul>
+                    </div>
+                </div>
+                <ul className='w-full xl:w-auto xl:flex-grow h-full flex flex-col text-gray-400 text-[20px]'>
+                    <li className='py-5 border-t border-white border-opacity-20'>Conceptualization</li>
+                    <li className='py-5 border-t border-white border-opacity-20'>UI/UX Design</li>
+                    <li className='py-5 border-t border-white border-opacity-20'>Front End Development</li>
+                    <li className='py-5 border-y border-white border-opacity-20'>Back End Development</li>
+                </ul>
+            </div>
+            {/* <div className="relative mt-[160px] w-full flex sm:hidden flex-col xl:flex-row gap-10 xl:gap-20 items-center z-20">
+                <div className="flex flex-col w-full xl:w-[620px]">
+                    <h1 className="text-white text-[40px] md:text-[56px] font-semibold">
                         FC 24 Player Database
                     </h1>
                     <p className="font-medium text-[20px] text-custom-gray mt-8">
@@ -85,13 +139,13 @@ export default function Banner() {
                         </ul>
                     </div>
                 </div>
-                <ul className='w-[300px] h-full flex flex-col text-gray-400 text-[20px]'>
+                <ul className='w-full xl:w-auto xl:flex-grow h-full flex flex-col text-gray-400 text-[20px]'>
                     <li className='py-5 border-t border-white border-opacity-20'>Conceptualization</li>
                     <li className='py-5 border-t border-white border-opacity-20'>UI/UX Design</li>
                     <li className='py-5 border-t border-white border-opacity-20'>Front End Development</li>
                     <li className='py-5 border-y border-white border-opacity-20'>Back End Development</li>
                 </ul>
-            </div>
+            </div> */}
         </div>
     );
 }

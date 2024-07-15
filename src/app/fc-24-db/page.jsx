b5594ed1b9cd98a8d29ee8fb723678c480a71e5e
DWC97 <dwcarton1@gmail.com>
2024-07-15 10:46:50 +0100

    banner image

--- src/app/fc-24-db/page.jsx
+++ src/app/fc-24-db/page.jsx
@@ -2,8 +2,8 @@ import Banner from "@/components/project-page/Banner";
 
 export default function FC24DB(){
     return (
-        <div className="w-full  flex flex-col justify-center items-center ">
-            <div className="px-20 max-w-[1100px] w-full">
+        <div className="w-full h-[200vh] flex flex-col items-center relative">
+            <div className="max-w-[1050px] w-full absolute px-20">
             <Banner />
             </div>
        
--- src/components/project-page/Banner.jsx
+++ src/components/project-page/Banner.jsx
@@ -3,16 +3,27 @@
 import { useEffect, useState } from 'react';
 import { motion } from 'framer-motion';
 import projectsData from '@/data/projects.json';
+import Image from 'next/image';
 
 export default function Banner() {
     const project = projectsData.projects[2];
-    
 
     return (
-        <div className="h-screen bg-red-700 w-full">
-            <div className=''>
-
+        <div className='w-full h-[700px]'>
+            <div className="relative h-[530px] w-full -rotate-3 z-0">
+                <Image
+                    src="/images/fc-24-player-database/saka-header.avif"
+                    alt="banner"
+                    fill
+                    sizes="100vw"
+                    priority
+                    className="object-contain opacity-90"
+                />
+                
             </div>
+            <div className='absolute bottom-0 left-0 w-full h-[250px] bg-primary-dark' />
+            <div className='absolute bottom-[250px] left-0 w-full h-[350px] bg-gradient-to-b from-transparent to-primary-dark' />
+
             
         </div>
     );

b36929be3975974f76ea5c382b0c14d64c05c6b4
DWC97 <dwcarton1@gmail.com>
2024-07-17 10:11:26 +0100

    banner imports project json

--- src/app/fc-24-db/page.jsx
+++ src/app/fc-24-db/page.jsx
@@ -8,7 +8,7 @@ export default function FC24DB() {
     return (
         <div className="w-full flex flex-col items-center relative">
             <div className="max-w-[1160px] w-full px-10 sm:px-20 mb-10 sm:-mb-10">
-                <Banner />
+                <Banner index={2} />
             </div>
 
             <div className="max-w-[1160px] w-full px-10 sm:px-20 pb-20  z-30">
--- src/components/project-page/Banner.jsx
+++ src/components/project-page/Banner.jsx
@@ -4,8 +4,8 @@ import { useEffect, useState } from 'react';
 import projectsData from '@/data/projects.json';
 import Image from 'next/image';
 
-export default function Banner() {
-    const project = projectsData.projects[2];
+export default function Banner({ index }) {
+    const project = projectsData.projects[index];
     const [opacity, setOpacity] = useState(1);
 
     useEffect(() => {
@@ -26,7 +26,7 @@ export default function Banner() {
             style={{ opacity }}
             >
                 <Image
-                    src="/images/fc-24-player-database/saka-header.avif"
+                    src={project.banner}
                     alt="banner"
                     fill
                     sizes="100vw"
@@ -40,13 +40,10 @@ export default function Banner() {
             <div className=" sm:absolute relative sm:mt-0 mt-[160px] flex  sm:top-[320px] w-full  flex-col xl:flex-row gap-10 xl:gap-20 items-center z-20">
                 <div className="flex flex-col w-full xl:w-[620px]">
                     <h1 className="text-white text-[40px] md:text-[56px] font-semibold">
-                        FC 24 Player Database
+                        {project.title}
                     </h1>
                     <p className="font-medium text-[20px] text-custom-gray mt-8">
-                        Search & visualization tool for the FC 24 player
-                        dataset. Includes ~5000 players from Europe&apos;s top five
-                        leagues. Non-commerical product created purely for skill
-                        development.
+                        {project.desc}
                     </p>
                     <div className="flex flex-row justify-between items-center mt-8">
                         <div className="flex flex-row items-center gap-3 hover:opacity-80 ease-in-out duration-300">
@@ -85,67 +82,14 @@ export default function Banner() {
                         </ul>
                     </div>
                 </div>
-                <ul className='w-full xl:w-auto xl:flex-grow h-full flex flex-col text-gray-400 text-[20px]'>
-                    <li className='py-5 border-t border-white border-opacity-20'>Conceptualization</li>
-                    <li className='py-5 border-t border-white border-opacity-20'>UI/UX Design</li>
-                    <li className='py-5 border-t border-white border-opacity-20'>Front End Development</li>
-                    <li className='py-5 border-y border-white border-opacity-20'>Back End Development</li>
+                <ul className='w-full xl:w-auto xl:flex-grow h-full flex flex-col text-gray-400 text-[20px] border-b border-white border-opacity-20'>
+                    {project.roles.map(role => {
+                        return (
+                            <li className='py-5 border-t border-white border-opacity-20'>{role}</li>
+                        )
+                    })}
                 </ul>
             </div>
-            {/* <div className="relative mt-[160px] w-full flex sm:hidden flex-col xl:flex-row gap-10 xl:gap-20 items-center z-20">
-                <div className="flex flex-col w-full xl:w-[620px]">
-                    <h1 className="text-white text-[40px] md:text-[56px] font-semibold">
-                        FC 24 Player Database
-                    </h1>
-                    <p className="font-medium text-[20px] text-custom-gray mt-8">
-                        Search & visualization tool for the FC 24 player
-                        dataset. Includes ~5000 players from Europe's top five
-                        leagues. Non-commerical product created purely for skill
-                        development.
-                    </p>
-                    <div className="flex flex-row justify-between items-center mt-8">
-                        <div className="flex flex-row items-center gap-3 hover:opacity-80 ease-in-out duration-300">
-                            <span className="text-dark-blue font-medium text-[20px]">
-                                Live demo
-                            </span>
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                width={24}
-                                height={24}
-                                viewBox="0 0 16 16"
-                            >
-                                <path
-                                    fill="#00EEFF"
-                                    fillRule="evenodd"
-                                    d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8"
-                                ></path>
-                            </svg>
-                        </div>
-                        <ul className="flex flex-row ">
-                            {project.stack.map((stack, i) => {
-                                return (
-                                    <li
-                                        key={i}
-                                        className="-mr-2 last:mr-0 h-[48px] w-[48px] flex justify-center items-center rounded-full border border-custom-gray border-opacity-30 bg-gradient-to-r from-primary-light to-primary-dark"
-                                    >
-                                        <div
-                                            dangerouslySetInnerHTML={{
-                                                __html: stack.svg,
-                                            }}
-                                        />
-                                    </li>
-                                );
-                            })}
-                        </ul>
-                    </div>
-                </div>
-                <ul className='w-full xl:w-auto xl:flex-grow h-full flex flex-col text-gray-400 text-[20px]'>
-                    <li className='py-5 border-t border-white border-opacity-20'>Conceptualization</li>
-                    <li className='py-5 border-t border-white border-opacity-20'>UI/UX Design</li>
-                    <li className='py-5 border-t border-white border-opacity-20'>Front End Development</li>
-                    <li className='py-5 border-y border-white border-opacity-20'>Back End Development</li>
-                </ul>
-            </div> */}
         </div>
     );
 }

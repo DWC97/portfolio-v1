16c7fdb1761c54cf2ff4292d2a93012aaa9e7377
DWC97 <dwcarton1@gmail.com>
2024-07-19 10:12:30 +0100

    test2 component created

---
+++ src/app/test2/page.jsx
@@ -0,0 +1,9 @@
+import Hero from "@/components/home/hero/Hero";
+
+export default function Test2(){
+    return (
+        <div className="w-full min-h-screen">
+            <Hero />
+        </div>
+    )
+}

26e38109431407d661ef80dbde216a2b7516e5ac
DWC97 <dwcarton1@gmail.com>
2024-07-19 13:47:11 +0100

    layout shift fix attempt 1

--- src/components/home/hero/Hero.jsx
+++ src/components/home/hero/Hero.jsx
@@ -1,12 +1,6 @@
 'use client';
 
-import React, {
-    useEffect,
-    useRef,
-    useState,
-    useMemo,
-    Suspense,
-} from 'react';
+import React, { useEffect, useRef, useState, useMemo, Suspense } from 'react';
 
 import DecodeAnimation from 'react-decode-animation';
 import { motion } from 'framer-motion';
@@ -40,7 +34,6 @@ export default function Hero() {
     const scrollAnimationRef = useRef(null);
     const [opacity, setOpacity] = useState(0);
     const viewportWidth = useViewportWidth();
-
 
     useEffect(() => {
         if (modelReady) {
@@ -82,68 +75,75 @@ export default function Hero() {
 
     return (
         <div className="h-screen w-full relative flex justify-center items-center">
-            {divVisible && (
-                <motion.div
-                    initial={{ width: 0 }}
-                    animate={{ width: divStyles.width }}
-                    transition={{ duration: 1.5, ease: [0.4, 0, 0.1, 1] }}
-                    style={{ height: divStyles.height }}
-                    className="z-10 backdrop-blur-sm relative flex flex-col px-8 overflow-hidden select-none"
-                >
-                    <div className="absolute top-0 left-0 z-10 w-full h-full bg-gradient-to-b from-med-blue to-[#143A3A] opacity-10" />
-                    <div className="absolute top-0 left-0 w-full">
-                        <h2
-                            className={`z-20 text-[14px] sm:text-[20px] 2xl:text-[24px] ${playing ? 'text-dark-blue weak-glow' : 'text-transparent'}  font-semibold tracking-widest pt-6  px-7 sm:px-8`}
-                        >
-                       
+            <div
+                style={{
+                    width: divStyles.width,
+                    height: divStyles.height,
+                    position: 'relative',
+                    overflow: 'hidden',
+                }}
+            >
+                {divVisible && (
+                    <motion.div
+                        initial={{ width: 0 }}
+                        animate={{ width: divStyles.width }}
+                        transition={{ duration: 1.5, ease: [0.4, 0, 0.1, 1] }}
+                        style={{ height: divStyles.height }}
+                        className="z-10 backdrop-blur-sm relative flex flex-col px-8 overflow-hidden select-none"
+                    >
+                        <div className="absolute top-0 left-0 z-10 w-full h-full bg-gradient-to-b from-med-blue to-[#143A3A] opacity-10" />
+                        <div className="absolute top-0 left-0 w-full">
+                            <h2
+                                className={`z-20 text-[14px] sm:text-[20px] 2xl:text-[24px] ${playing ? 'text-dark-blue weak-glow' : 'text-transparent'}  font-semibold tracking-widest pt-6  px-7 sm:px-8`}
+                            >
                                 <DecodeAnimation
                                     ref={nameRef}
                                     text={'DOUGLAS WILLIAM CARTON'}
                                     customCharacters="アイウエオカキクケコサシスセソタチツテトナニヌネノハヒフヘホマミムメモヤユヨーラリルレロワヰヱヲンガギグゲゴザジズゼゾダヂヅデドバビブベボパピプペポ"
                                 />
-                        
-                        </h2>
-                    </div>
+                            </h2>
+                        </div>
 
-                    <h1
-                        className={`z-20 font-medium text-[44px] sm:text-[88px] 2xl:text-[116px] mt-[44px]  sm:mt-[36px] sm:-mb-6 -ml-2 ${textVisible ? 'text-white ' : 'text-transparent'} ease-in-out transition-colors duration-300`}
-                    >
-                        Developer
-                    </h1>
-                    <h1
-                        className={`z-20 font-medium text-[44px] sm:text-[88px] 2xl:text-[116px] -my-4 sm:-my-6 -ml-2 ease-in-out transition-colors duration-500 ${textVisible ? 'text-white ' : 'text-transparent'}`}
-                    >
-                        <span
-                            className={`${textVisible ? 'text-custom-gray' : 'text-transparent'} ease-in-out transition-colors duration-500`}
+                        <h1
+                            className={`z-20 font-medium text-[44px] sm:text-[88px] 2xl:text-[116px] mt-[44px]  sm:mt-[36px] sm:-mb-6 -ml-2 ${textVisible ? 'text-white ' : 'text-transparent'} ease-in-out transition-colors duration-300`}
                         >
-                            +&nbsp;
-                        </span>
-                        <span
-                            className={`${textVisible ? 'text-white' : 'text-transparent'} ease-in-out transition-colors duration-500`}
+                            Developer
+                        </h1>
+                        <h1
+                            className={`z-20 font-medium text-[44px] sm:text-[88px] 2xl:text-[116px] -my-4 sm:-my-6 -ml-2 ease-in-out transition-colors duration-500 ${textVisible ? 'text-white ' : 'text-transparent'}`}
                         >
-                            <Suspense fallback={null}>
-                                <TypeAnimation
-                                    sequence={[
-                                        'Designer',
-                                        3000,
-                                        'Engineer',
-                                        3000,
-                                        'Researcher',
-                                        3000,
-                                    ]}
-                                    wrapper="span"
-                                    speed={10}
-                                    omitDeletionAnimation={true}
-                                    deletionSpeed={30}
-                                    repeat={Infinity}
-                                    cursor={true}
-                                    preRenderFirstString={false}
-                                />
-                            </Suspense>
-                        </span>
-                    </h1>
-                </motion.div>
-            )}
+                            <span
+                                className={`${textVisible ? 'text-custom-gray' : 'text-transparent'} ease-in-out transition-colors duration-500`}
+                            >
+                                +&nbsp;
+                            </span>
+                            <span
+                                className={`${textVisible ? 'text-white' : 'text-transparent'} ease-in-out transition-colors duration-500`}
+                            >
+                                <Suspense fallback={null}>
+                                    <TypeAnimation
+                                        sequence={[
+                                            'Designer',
+                                            3000,
+                                            'Engineer',
+                                            3000,
+                                            'Researcher',
+                                            3000,
+                                        ]}
+                                        wrapper="span"
+                                        speed={10}
+                                        omitDeletionAnimation={true}
+                                        deletionSpeed={30}
+                                        repeat={Infinity}
+                                        cursor={true}
+                                        preRenderFirstString={false}
+                                    />
+                                </Suspense>
+                            </span>
+                        </h1>
+                    </motion.div>
+                )}
+            </div>
 
             {/* <div
                 className={`absolute ${

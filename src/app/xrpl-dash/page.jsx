be2e213b103a0c85da5957d0e1521116e123544e
DWC97 <dwcarton1@gmail.com>
2024-07-24 10:34:31 +0100

    projects json updated with tech stack logos

--- src/app/xrpl-dash/page.jsx
+++ src/app/xrpl-dash/page.jsx
@@ -1,7 +1,10 @@
-export default function XRPLDash(){
+export default function XRPLDash() {
     return (
-        <div className="w-full min-h-screen">
-            <h1 className="text-white text-5xl">welcome to dash</h1>
+        <div className="w-full flex flex-col items-center relative">
+            <div className="max-w-[1160px] w-full px-10 sm:px-20 mb-10 sm:-mb-10">
+                <Banner index={0} />
+            </div>
+            <div className="max-w-[1160px] w-full px-10 sm:px-20 pb-20  z-30"></div>
         </div>
-    )
+    );
 }

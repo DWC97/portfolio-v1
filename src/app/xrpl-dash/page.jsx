763a5b4c292e7333e23bb7a6d062d35f73987db9
DWC97 <dwcarton1@gmail.com>
2024-07-24 11:47:22 +0100

    markup p2

--- src/app/xrpl-dash/page.jsx
+++ src/app/xrpl-dash/page.jsx
@@ -186,11 +186,12 @@ export default function XRPLDash() {
                         It's all well and good having a tonne of features for
                         the user but how can you realistically fit them into a
                         UI without it getting cluttered? The solution we came up
-                        with was to make our platform a dashboard with a <span className="text-dark-blue">modular
-                        layout</span>. This way, the user can pick and choose what
-                        tools and assets are useful for them to create a custom
-                        interface. For the MVP, we wanted to offer the user the
-                        most important trading/tracking modules:
+                        with was to make our platform a dashboard with a{' '}
+                        <span className="text-dark-blue">modular layout</span>.
+                        This way, the user can pick and choose what tools and
+                        assets are useful for them to create a custom interface.
+                        For the MVP, we wanted to offer the user the most
+                        important trading/tracking modules:
                     </p>
                     <ul className="pl-4 pt-6 text-dark-blue text-[20px]">
                         <li className="flex flex-row gap-4 items-center pb-2">
@@ -306,7 +307,17 @@ export default function XRPLDash() {
                         />
                     </div>
                     <p className="font-medium text-[20px] text-gray-200 leading-relaxed">
-                    <span className="text-dark-blue">Material UI</span> is used for charting and the module cards UI. This pairs well with <span className="text-dark-blue">React-Grid-Layout</span> which enables drag and drop functionality for each module. We built a custom React hook for resizing the modules too. <span className="text-dark-blue">Framer Motion</span> is used to ensure clean transitions between states.
+                        <span className="text-dark-blue">Material UI</span> is
+                        used for charting and the module cards UI. This pairs
+                        well with{' '}
+                        <span className="text-dark-blue">
+                            React-Grid-Layout
+                        </span>{' '}
+                        which enables drag and drop functionality for each
+                        module. We built a custom React hook for resizing the
+                        modules too.{' '}
+                        <span className="text-dark-blue">Framer Motion</span> is
+                        used to ensure clean transitions between states.
                     </p>
                     <div className="w-full relative mt-10 mb-8 md:px-20">
                         <Image
@@ -324,11 +335,198 @@ export default function XRPLDash() {
                 </div>
                 <div id="data" className="pt-10">
                     <h1 className="font-semibold text-white text-[40px] pb-4">
-                    Managing on-chain data
+                        Managing on-chain data
                     </h1>
                     <p className="font-medium text-[20px] text-gray-200 leading-relaxed">
-                    The biggest challenge for the project has been managing and delivering quality data to the dashboard front-end. Our main data source is our node on the XRPL network. We utilize <span className="text-dark-blue">Next.js server actions</span> alongside an <span className="text-dark-blue">Express</span> server which does the heavy lifitng. Certain methods are performed by serverless API routes (login, trading) whilst the main Express server is used for fetching large amounts of data from the node (on-chain data) and database (user data) quickly. <span className="text-dark-blue">Redux</span> is used for state management to better organize state so some of it can be cached in local storage while more important data goes to the Supabase (<span className="text-dark-blue">Postgresql</span>) database.
+                        The biggest challenge for the project has been managing
+                        and delivering quality data to the dashboard front-end.
+                        Our main data source is our node on the XRPL network. We
+                        utilize{' '}
+                        <span className="text-dark-blue">
+                            Next.js server actions
+                        </span>{' '}
+                        alongside an{' '}
+                        <span className="text-dark-blue">Express</span> server
+                        which does the heavy lifitng. Certain methods are
+                        performed by serverless API routes (login, trading)
+                        whilst the main Express server is used for fetching
+                        large amounts of data from the node (on-chain data) and
+                        database (user data) quickly.{' '}
+                        <span className="text-dark-blue">Redux</span> is used
+                        for state management to better organize state so some of
+                        it can be cached in local storage while more important
+                        data goes to the Supabase (
+                        <span className="text-dark-blue">Postgresql</span>)
+                        database.
                     </p>
+                </div>
+                <div id="features" className="pt-10">
+                    <h1 className="font-semibold text-white text-[40px] pb-4">
+                        Other features
+                    </h1>
+                    <ul className="pl-4 ">
+                        <li className="pb-2">
+                            <div className="flex flex-row gap-4 items-center pb-2">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width={24}
+                                    height={24}
+                                    viewBox="0 0 1024 1024"
+                                >
+                                    <path
+                                        fill="#00EEFF"
+                                        d="M452.864 149.312a29.12 29.12 0 0 1 41.728.064L826.24 489.664a32 32 0 0 1 0 44.672L494.592 874.624a29.12 29.12 0 0 1-41.728 0a30.59 30.59 0 0 1 0-42.752L764.736 512L452.864 192a30.59 30.59 0 0 1 0-42.688m-256 0a29.12 29.12 0 0 1 41.728.064L570.24 489.664a32 32 0 0 1 0 44.672L238.592 874.624a29.12 29.12 0 0 1-41.728 0a30.59 30.59 0 0 1 0-42.752L508.736 512L196.864 192a30.59 30.59 0 0 1 0-42.688"
+                                    ></path>
+                                </svg>
+                                <span className="text-dark-blue font-medium text-[20px]">
+                                    Trading via the DEX
+                                </span>
+                            </div>
+                            <p className="font-medium text-[20px] text-gray-200 leading-relaxed pl-10">
+                                There are a number of options for trading
+                                including quick trade (market buy/sell) or limit
+                                orders. The dash facilitates trading between any
+                                two assets running on the XRPL using{' '}
+                                <span className="text-dark-blue">
+                                    serverless API route handlers
+                                </span>{' '}
+                                and the Xaman API. XRP, the native currency of
+                                the ledger is used as an intermediary bridge
+                                asset with pathfinding.
+                            </p>
+                        </li>
+                        <li className="pb-2 pt-2">
+                            <div className="flex flex-row gap-4 items-center pb-2">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width={24}
+                                    height={24}
+                                    viewBox="0 0 1024 1024"
+                                >
+                                    <path
+                                        fill="#00EEFF"
+                                        d="M452.864 149.312a29.12 29.12 0 0 1 41.728.064L826.24 489.664a32 32 0 0 1 0 44.672L494.592 874.624a29.12 29.12 0 0 1-41.728 0a30.59 30.59 0 0 1 0-42.752L764.736 512L452.864 192a30.59 30.59 0 0 1 0-42.688m-256 0a29.12 29.12 0 0 1 41.728.064L570.24 489.664a32 32 0 0 1 0 44.672L238.592 874.624a29.12 29.12 0 0 1-41.728 0a30.59 30.59 0 0 1 0-42.752L508.736 512L196.864 192a30.59 30.59 0 0 1 0-42.688"
+                                    ></path>
+                                </svg>
+                                <span className="text-dark-blue font-medium text-[20px]">
+                                    Security and auth
+                                </span>
+                            </div>
+                            <div className="font-medium text-[20px] text-gray-200 leading-relaxed pl-10">
+                                <p>
+                                    Most of our features are custom to the user
+                                    so we require the user to login with their
+                                    Xaman wallet to access the dash. Xaman is a
+                                    non-custodial wallet service for users to
+                                    store their XRPL assets. Our dashboard
+                                    interacts with the Xaman API to authenticate
+                                    the user's signature and then return to us
+                                    their XRPL address.{' '}
+                                    <span className="text-dark-blue">
+                                        NextAuth.js
+                                    </span>{' '}
+                                    is used with{' '}
+                                    <span className="text-dark-blue">
+                                        React-Cookies
+                                    </span>{' '}
+                                    for session management and route protection.
+                                </p>
+                                <div className="w-full relative mt-10 mb-8 md:px-20">
+                                    <Image
+                                        src={'/images/jr/contact.avif'}
+                                        alt="scene img"
+                                        width={0}
+                                        height={0}
+                                        sizes="100vw"
+                                        style={{
+                                            width: '100%',
+                                            height: '100%',
+                                        }}
+                                    />
+                                </div>
+                            </div>
+                        </li>
+                        <li className="pb-2 pt-2">
+                            <div className="flex flex-row gap-4 items-center pb-2">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width={24}
+                                    height={24}
+                                    viewBox="0 0 1024 1024"
+                                >
+                                    <path
+                                        fill="#00EEFF"
+                                        d="M452.864 149.312a29.12 29.12 0 0 1 41.728.064L826.24 489.664a32 32 0 0 1 0 44.672L494.592 874.624a29.12 29.12 0 0 1-41.728 0a30.59 30.59 0 0 1 0-42.752L764.736 512L452.864 192a30.59 30.59 0 0 1 0-42.688m-256 0a29.12 29.12 0 0 1 41.728.064L570.24 489.664a32 32 0 0 1 0 44.672L238.592 874.624a29.12 29.12 0 0 1-41.728 0a30.59 30.59 0 0 1 0-42.752L508.736 512L196.864 192a30.59 30.59 0 0 1 0-42.688"
+                                    ></path>
+                                </svg>
+                                <span className="text-dark-blue font-medium text-[20px]">
+                                    User personalization
+                                </span>
+                            </div>
+                            <div className="font-medium text-[20px] text-gray-200 leading-relaxed pl-10">
+                                <p>
+                                    Once logged in to the dash, the user can
+                                    customize their dashboard module layout and
+                                    save it. They also have the option of
+                                    choosing a profile picture + banner from
+                                    their NFT collection and writing a personal
+                                    bio. This gives their wallet an identity.
+                                    They can also search for other users based
+                                    on their wallet address and view their
+                                    profile.
+                                </p>
+                                <div className="w-full relative mt-10 mb-8 md:px-20">
+                                    <Image
+                                        src={'/images/jr/contact.avif'}
+                                        alt="scene img"
+                                        width={0}
+                                        height={0}
+                                        sizes="100vw"
+                                        style={{
+                                            width: '100%',
+                                            height: '100%',
+                                        }}
+                                    />
+                                </div>
+                            </div>
+                        </li>
+                        <li className="pb-2 pt-2">
+                            <div className="flex flex-row gap-4 items-center pb-2">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width={24}
+                                    height={24}
+                                    viewBox="0 0 1024 1024"
+                                >
+                                    <path
+                                        fill="#00EEFF"
+                                        d="M452.864 149.312a29.12 29.12 0 0 1 41.728.064L826.24 489.664a32 32 0 0 1 0 44.672L494.592 874.624a29.12 29.12 0 0 1-41.728 0a30.59 30.59 0 0 1 0-42.752L764.736 512L452.864 192a30.59 30.59 0 0 1 0-42.688m-256 0a29.12 29.12 0 0 1 41.728.064L570.24 489.664a32 32 0 0 1 0 44.672L238.592 874.624a29.12 29.12 0 0 1-41.728 0a30.59 30.59 0 0 1 0-42.752L508.736 512L196.864 192a30.59 30.59 0 0 1 0-42.688"
+                                    ></path>
+                                </svg>
+                                <span className="text-dark-blue font-medium text-[20px]">
+                                Messaging system
+                                </span>
+                            </div>
+                            <div className="font-medium text-[20px] text-gray-200 leading-relaxed pl-10">
+                                <p>
+                                The messaging system utilizes <span className="text-dark-blue">web sockets</span> for real-time communication between users. Users can send/receive messages, see previous conversations and search through old chats. They have the option of sending the message as an XRPL transaction with or without payment for a minimal fee. This provides a way of sending money/messages on a blockchain with transparency and immutability.
+                                </p>
+                                <div className="w-full relative mt-10 mb-8 md:px-20">
+                                    <Image
+                                        src={'/images/jr/contact.avif'}
+                                        alt="scene img"
+                                        width={0}
+                                        height={0}
+                                        sizes="100vw"
+                                        style={{
+                                            width: '100%',
+                                            height: '100%',
+                                        }}
+                                    />
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
                 </div>
             </div>
             <Footer />
